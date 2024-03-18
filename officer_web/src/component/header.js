04eee1bb93985dcc2368a4f159759b5a192f2780
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-03-18 18:12:41 +0530

    ddddd

--- officer_web/src/component/header.js
+++ officer_web/src/component/header.js
@@ -3,18 +3,39 @@ import { Link, NavLink } from 'react-router-dom';
 import './header.css';
 import Logo from '../asset/logo.png';
 
+import axios from 'axios';
+
 function Header() {
     const [showMessageBox, setShowMessageBox] = useState(false);
     const [criminalInfo, setCriminalInfo] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    const [notificationData, setNotificationData] = useState(null); // State to store notification count
+    const token = localStorage.getItem('token');
     useEffect(() => {
+        // Fetch notification details
+        const fetchNotificationDetails = async () => {
+            try {
+                const response = await axios.post('/api/officer/get-alerts',{
+                    headers: {
+                      'Authorization': 'Bearer ${token}'
+                    }});
+                // Assuming the response data includes the notification count
+                const { data } = response.data;
+                setNotificationData(data);
+            } catch (error) {
+                console.error('Error fetching notification details:', error);
+            }
+        };
+
+        // Call the function to fetch notification details
+        fetchNotificationDetails();
+
         // Simulating an API call to check if a criminal is found
         const timeout = setTimeout(() => {
             const criminalData = {
-                name: 'John Doe',
-                location: 'City X',
-                time: 'Today at 3:00 PM'
+                name: notificationData[1],
+                location: notificationData[2],
+                time: notificationData[3]
             };
             setCriminalInfo(criminalData);
             setShowMessageBox(true); // Show the message box
@@ -23,6 +44,8 @@ function Header() {
         // Clean up the timeout to avoid memory leaks
         return () => clearTimeout(timeout);
     }, []); // Run this effect only once when the component mounts
+
+
 
     const closeMessageBox = () => {
         setShowMessageBox(false);

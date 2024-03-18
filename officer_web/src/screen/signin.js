28fd55332d510542ff54f800a1ba9ee74c581759
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-03-19 01:50:44 +0530

    Update signin.js

--- officer_web/src/screen/signin.js
+++ officer_web/src/screen/signin.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom'; // Import useHistory hook
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import './signin.css';
 
 function SignIn() {
-    const history = useHistory(); // Initialize useHistory hook
+    const history = useHistory();
 
     const [formData, setFormData] = useState({
         username: '',
@@ -22,33 +22,28 @@ function SignIn() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            // Create FormData object
             const formData1 = new FormData();
             formData1.append('username', formData.username);
             formData1.append('password', formData.password);
 
-            // Make POST request to the login endpoint
             const response = await axios.post('http://127.0.0.1:8000/api/auth/login', formData1, {
                 headers: {
-                    'Content-Type': 'multipart/form-data' // Set content type to multipart/form-data
+                    'Content-Type': 'multipart/form-data'
                 }
             });
 
             console.log('Response:', response.data);
 
-            // Clear the form fields after successful login
             setFormData({
                 username: '',
                 password: ''
             });
 
-            // Navigate to the home page
+            // Redirect to the home page
             history.push('/');
 
-            // Optionally, you can handle success behavior here, e.g., displaying a success message
         } catch (error) {
             console.error('Error:', error);
-            // Optionally, you can handle error behavior here, e.g., displaying an error message to the user
         }
     };
 
